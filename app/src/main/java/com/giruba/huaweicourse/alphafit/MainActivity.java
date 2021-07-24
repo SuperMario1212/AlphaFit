4a1ab773aecd08e26375c61f5765c504b2eb1b19
Giruba （ぎるば） <ngzhekai@gmail.com>
2021-07-24 21:32:16 +0800

    Login Layout added

--- app/src/main/java/com/giruba/huaweicourse/alphafit/MainActivity.java
+++ app/src/main/java/com/giruba/huaweicourse/alphafit/MainActivity.java
@@ -65,8 +65,8 @@ public class MainActivity extends AppCompatActivity {
         checklist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, CheckList.class);
-//                startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, CheckList.class);
+                startActivity(intent);
             }
         });
     }
