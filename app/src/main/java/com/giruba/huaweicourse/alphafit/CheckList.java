8ca9b81bb9d5180bd2c90994a4237741ca74b0ab
Giruba （ぎるば） <ngzhekai@gmail.com>
2021-08-01 15:06:20 +0800

    Updated the workout timer

--- app/src/main/java/com/giruba/huaweicourse/alphafit/CheckList.java
+++ app/src/main/java/com/giruba/huaweicourse/alphafit/CheckList.java
@@ -3,6 +3,7 @@ package com.giruba.huaweicourse.alphafit;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
@@ -11,124 +12,173 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class CheckList extends AppCompatActivity {
 
-    private ListView citylist;
-    private EditText editcity;
-    private Button addcity;
-    private Button sort_ascending;
-    private Button sort_descending;
-    private Button clearall;
-    ArrayAdapter<String> adapt;
-    ArrayList<String> cities = new ArrayList<>();
+    Button btn_add,stopStartButton;
+    EditText et_workout;
+    TextView timerText;
+
+    boolean timerStarted = false;
+
+    Timer timer;
+    TimerTask timerTask;
+    Double time = 0.0;
+
+
+    ArrayAdapter workoutArrayAdapter;
+    DataBaseHelper dataBaseHelper;
+    ListView lv_workoutList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_list);
 
-        citylist = findViewById(R.id.citieslist);
-        editcity = findViewById(R.id.addcities);
-        addcity = findViewById(R.id.addbutton);
-        sort_ascending = findViewById(R.id.ascending);
-        sort_descending = findViewById(R.id.descending);
-        clearall = findViewById(R.id.clearlist);
+        btn_add = findViewById(R.id.btn_add);
+        et_workout = findViewById(R.id.et_workout);
+        lv_workoutList = findViewById(R.id.workout_list);
+        timerText = findViewById(R.id.timerText);
+        stopStartButton = findViewById(R.id.startStopButton);
+        timer = new Timer();
 
+        dataBaseHelper = new DataBaseHelper(CheckList.this);
+        ShowWorkoutOnListView(dataBaseHelper);
 
-
-        //user input add cities
-        addcity.setOnClickListener(new View.OnClickListener() {
+        btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String cityname = editcity.getText().toString();
-                editcity.getText().clear(); //after type, clear the line
-                if(cityname.length()>0)
-                {
-                    //DATABASE
+                WorkoutTypes workout;
 
-                    //common query
-                    cities.add(cityname);
-                    citylist.setAdapter(adapt); //make new row
-                    adapt.notifyDataSetChanged();
+                try{
+                    workout = new WorkoutTypes(-1,et_workout.getText().toString());
+                    et_workout.getText().clear();
+                    Toast.makeText(CheckList.this, workout.getWorkout() + " added", Toast.LENGTH_SHORT).show();
+                }catch(Exception e){
+                    Toast.makeText(CheckList.this, "Error adding workout item", Toast.LENGTH_SHORT).show();
+                    workout = new WorkoutTypes(-1,"err");
                 }
+
+                DataBaseHelper dataBaseHelper = new DataBaseHelper(CheckList.this);
+                boolean success = dataBaseHelper.addOne(workout);
+                ShowWorkoutOnListView(dataBaseHelper);
+
+                Toast.makeText(CheckList.this, "Success", Toast.LENGTH_SHORT).show();
+
+                ShowWorkoutOnListView(dataBaseHelper);
+
             }
         });
 
-        //convert to ascending
-        sort_ascending.setOnClickListener(new View.OnClickListener() {
+        lv_workoutList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(CheckList.this, "Sorted in ascending order", Toast.LENGTH_SHORT).show();
-                Collections.sort(cities);
-                adapt.notifyDataSetChanged();
-            }
-        });
-
-        //sort by descending order, can revert back to ascending
-        sort_descending.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Collections.reverse(cities); //able to revert back to ascending
-                adapt.notifyDataSetChanged();
-                Toast.makeText(CheckList.this, "Press again to convert back", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        //delete items in list
-        citylist.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                WorkoutTypes clickedWorkouts = (WorkoutTypes) parent.getItemAtPosition(position);
                 final int pos = position;
 
                 new AlertDialog.Builder(CheckList.this)
                         .setIcon(android.R.drawable.ic_delete)
-                        .setTitle("Delete " +cities.get(position)+" ?" )
+                        .setTitle("Delete this ?")
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                String city = cities.get(pos); //store the city name in here first(prev position)
-                                cities.remove(pos);
-                                Toast.makeText(CheckList.this, city + " Deleted", Toast.LENGTH_SHORT).show();
-                                adapt.notifyDataSetChanged();
+                                dataBaseHelper.DeleteOne(clickedWorkouts);
+                                ShowWorkoutOnListView(dataBaseHelper);
+                                Toast.makeText(CheckList.this, "Deleted", Toast.LENGTH_SHORT).show();
                             }
                         })
-                        .setNegativeButton("no",null)
+                        .setNegativeButton("no", null)
                         .show();
                 return true;
             }
         });
 
-        // clear everything in list
-        clearall.setOnClickListener(new View.OnClickListener() {
+    }
+
+    public void resetTapped(View view){
+        AlertDialog.Builder resetAlert = new AlertDialog.Builder(this);
+        resetAlert.setTitle("Reset Timer");
+        resetAlert.setMessage("Reset timer?");
+        resetAlert.setPositiveButton("RESET", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                new AlertDialog.Builder(CheckList.this)
-                        .setIcon(android.R.drawable.ic_delete)
-                        .setTitle("Delete All ?")
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                cities.clear();
-                                Toast.makeText(CheckList.this, "List Cleared", Toast.LENGTH_SHORT).show();
-                                adapt.notifyDataSetChanged();
-                            }
-                        })
-                        .setNegativeButton("No",null)
-                        .show();
+            public void onClick(DialogInterface dialog, int which) {
+
+                if(timerTask != null)
+                {
+                    timerTask.cancel();
+                    stopStartButton.setText("START");
+                    time = 0.0;
+                    timerStarted = false;
+                    timerText.setText(formatTime(0,0,0));
+                }
             }
         });
 
-        //adapter
-        adapt = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                cities
-        );
+        resetAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //null
+            }
+        });
+        resetAlert.show();
+    }
 
+    public void startStopTapped(View view){
+        if(timerStarted == false)
+        {
+            timerStarted = true;
+            stopStartButton.setText("STOP");
+
+
+            startTimer();
+        }else {
+            timerStarted = false;
+            stopStartButton.setText("START");
+
+            timerTask.cancel();
+        }
+    }
+
+    private void startTimer()
+    {
+        timerTask = new TimerTask() {
+            @Override
+            public void run()
+            {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        time++;
+                        timerText.setText(getTimerText());
+                    }
+                });
+            }
+
+        };
+        timer.scheduleAtFixedRate(timerTask,0,1000);
+    }
+
+    private String getTimerText()
+    {
+        int rounded = (int) Math.round(time);
+        int seconds = ((rounded % 86400) % 3600) % 60;
+        int minutes = ((rounded % 86400) % 3600) / 60;
+        int hours = ((rounded % 86400) / 3600);
+
+        return formatTime(seconds, minutes, hours);
+    }
+
+    private String formatTime(int seconds, int minutes, int hours){
+        return String.format("%02d",hours) + " : " + String.format("%02d",minutes) + " : " + String.format("%02d",seconds);
+    }
+
+    private void ShowWorkoutOnListView(DataBaseHelper dbhelper) {
+        workoutArrayAdapter = new ArrayAdapter<WorkoutTypes>(CheckList.this, android.R.layout.simple_list_item_1, dbhelper.getEveryOne());
+        lv_workoutList.setAdapter(workoutArrayAdapter);
     }
 }
